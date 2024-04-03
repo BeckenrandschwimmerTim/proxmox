d8f2f46771aa371478b2d122d091ffc115a41bae
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-03 15:17:00 +0200

    Create install_standard_tools.sh

---
+++ lxc/install_standard_tools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#################################################################
+# Install my needed packages on LXC if tagged with "apptag_tools"
+#################################################################
+
+source <(curl -s https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/main.func)
+
+header_info
+func_check_if_proxmox
+
+for container in $(pct list | awk '{if(NR>1) print $1}'); do tools=0 && containername=$(pct exec "$container" hostname) &&\
+    echo "CT: $container - $containername" &&\
+    pct config $container | grep -q "apptag_tools" && echo "Tools: true" && tools=1 || echo "Tools: false" &&\
+    if [ $tools -eq 1 ]; then pct exec $container -- bash -c "apt-get update"; fi
+    if [ $tools -eq 1 ]; then pct exec $container -- bash -c "apt-get install curl mc htop -y"; fi
+    echo ---; done
