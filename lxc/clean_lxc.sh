ec2c1ee0c87e573ba2e57d7be546f853a5d3cf24
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-03 16:30:18 +0200

    Update clean_lxc.sh

--- lxc/clean_lxc.sh
+++ lxc/clean_lxc.sh
@@ -12,6 +12,6 @@ func_check_if_proxmox
 for container in $(pct list | awk '{if(NR>1) print $1}'); do containername=$(pct exec "$container" hostname) &&\
     echo "CT: $container - $containername" &&\
     pct exec $container -- bash -c "apt-get -y --purge autoremove && apt-get -y autoclean" &&\
-    pct exec $container -- bash <(curl -fsSL https://github.com/tteck/Proxmox/raw/main/misc/clean.sh) &&\
+    pct exec $container -- bash <(curl -fsSL https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/clean.sh) &&\
     pct exec $container -- bash -c "rm -rf /var/lib/apt/lists/* && apt-get updaten" &&\
     echo ---; done
