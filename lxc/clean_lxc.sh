39a21ffe04f476d564a3b4b2e99d089c8a077a95
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-03 16:49:43 +0200

    Update clean_lxc.sh

--- lxc/clean_lxc.sh
+++ lxc/clean_lxc.sh
@@ -11,9 +11,10 @@ func_check_if_proxmox
 
 for container in $(pct list | awk '{if(NR>1) print $1}'); do containername=$(pct exec "$container" hostname) &&\
     echo "CT: $container - $containername" &&\
-    echo "Purging and autoclean apt" &&\
-    pct exec $container -- bash -c "apt-get -y --purge autoremove && apt-get -y autoclean" &&\
-    pct exec $container -- bash <(curl -fsSL https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/clean.sh) &&\
-    echo "Cleaning /var/lib/apt/lists/" &&\
-    pct exec $container -- bash -c "rm -rf /var/lib/apt/lists/* && apt-get update" &&\
+    # echo "Purging and autoclean apt" &&\
+    # pct exec $container -- bash -c "apt-get -y --purge autoremove && apt-get -y autoclean" &&\
+    # pct exec $container -- bash <(curl -fsSL https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/clean.sh) &&\
+    # echo "Cleaning /var/lib/apt/lists/" &&\
+    # pct exec $container -- bash -c "rm -rf /var/lib/apt/lists/* && apt-get update" &&\
+    pct exec $container -- bash -c "apt-get -y --purge autoremove && apt-get -y autoclean && bash <(curl -fsSL https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/clean.sh) && rm -rf /var/lib/apt/lists/* && apt-get update"    
     echo ---; done
