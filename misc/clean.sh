fb3a93dd7c748f5665e7b122fd6ebd0d042cda4e
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-03 16:42:17 +0200

    Update clean.sh

--- misc/clean.sh
+++ misc/clean.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo -e "Cleaning /var/cache/. \n"
 cache=$(find /var/cache/ -type f)
 if [[ -z "$cache" ]]; then
   echo -e "It appears there are no cached files on your system. \n"
@@ -10,6 +11,7 @@ else
   sleep 2
 fi
 
+echo -e "Cleaning /var/log/. \n"
 logs=$(find /var/log/ -type f)
 if [[ -z "$logs" ]]; then
   echo -e "It appears there are no logs on your system. \n"
