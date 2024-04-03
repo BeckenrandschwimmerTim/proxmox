2694a4748604a3904ae3f2b5797b01ff87dd732b
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-03 11:26:33 +0200

    Update pihole_update.sh

--- lxc/pihole_update.sh
+++ lxc/pihole_update.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+#########################################################
+Update PiHole as LXC when its tagged with "apptag_pihole"
+#########################################################
+
 clear
+if command ! -v pveversion >/dev/null 2>&1; then echo -e "⚠️  Has to be executed on Proxmox"; exit; fi
+
 for container in $(pct list | awk '{if(NR>1) print $1}'); do pihole=0 && containername=$(pct exec "$container" hostname) &&\
     echo "CT: $container - $containername" &&\
     pct config $container | grep -q "apptag_pihole" && echo "Pihole: true" && pihole=1 || echo "Pihole: false" &&\
