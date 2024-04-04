2257be60e2992ce8dfc4a120491650c41752dd5c
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-04 10:46:16 +0200

    Update install_standard_tools.sh

--- lxc/install_standard_tools.sh
+++ lxc/install_standard_tools.sh
@@ -13,5 +13,5 @@ for container in $(pct list | awk '{if(NR>1) print $1}'); do tools=0 && containe
     echo "CT: $container - $containername" &&\
     pct config $container | grep -q "apptag_tools" && echo "Tools: true" && tools=1 || echo "Tools: false" &&\
     if [ $tools -eq 1 ]; then pct exec $container -- bash -c "apt-get update"; fi
-    if [ $tools -eq 1 ]; then pct exec $container -- bash -c "apt-get install curl mc htop -y"; fi
+    if [ $tools -eq 1 ]; then pct exec $container -- bash -c "apt-get install curl mc htop sudo net-tools nano wget -y"; fi
     echo ---; done
