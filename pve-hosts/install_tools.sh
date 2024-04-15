ecf9229b193e28d8ad755340a763b3401e868ef9
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-15 15:00:18 +0200

    Update install_tools.sh

--- pve-hosts/install_tools.sh
+++ pve-hosts/install_tools.sh
@@ -4,7 +4,8 @@
 # Sync a datastore with another (here attached by USB)
 ######################################################
 
-source ./misc/main.func
+$(dirname "$PWD")
+source $(dirname "$PWD")/misc/main.func
 
 header_info
 func_check_if_pbs
