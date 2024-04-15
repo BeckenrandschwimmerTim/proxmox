6f8e3f0c8eaa5bc0de002eb60a102c9606d11c10
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-15 15:07:52 +0200

    Update and rename install_tools.sh to install_apps.sh

---
+++ pve-hosts/install_apps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+##################################
+# Install my needed apps and tools
+##################################
+
+source ../misc/main.func
+
+header_info
+func_check_if_pbs
+
+exit
