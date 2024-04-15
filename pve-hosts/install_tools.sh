305aaabbc179661590b93edf41229da9cb78660a
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-15 14:57:37 +0200

    Create install_tools.sh

---
+++ pve-hosts/install_tools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+######################################################
+# Sync a datastore with another (here attached by USB)
+######################################################
+
+source ./misc/main.func
+
+header_info
+func_check_if_pbs
+
+exit
