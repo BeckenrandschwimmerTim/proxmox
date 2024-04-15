ca1f7d22b2f8425752ab9f0d65a8108460772ace
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-15 16:14:55 +0200

    Create set_cpu_scaling_governor_performance.sh

---
+++ pve-hosts/set_cpu_scaling_governor_performance.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##################################
+# Install my needed apps and tools
+##################################
+
+# Search for main.func primarily localy else source it from the web
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+if [ -f $DIR/../misc/main.func ]; then
+  echo -e "Use local source"
+  source "$DIR/../misc/main.func"
+else
+  echo -e "Use web source"
+  source <(curl -s https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/main.func)
+fi
+
+header_info
+
+echo "performance" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
