73a2cff594a75f3d9d4af93d1b165874117839ae
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-15 15:35:42 +0200

    Update install_apps.sh

--- pve-hosts/install_apps.sh
+++ pve-hosts/install_apps.sh
@@ -10,4 +10,4 @@ source "$DIR/../misc/main.func"
 header_info
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive  apt-get install mc htop curl powertop -y
+DEBIAN_FRONTEND=noninteractive apt-get install mc htop curl powertop acpi-support acpid acpi -y
