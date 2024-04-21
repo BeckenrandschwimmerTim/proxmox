a60ba0304111cad5c46c047ed8ffca7f62c581ce
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-21 22:20:44 +0200

    Update clean_lxc.sh

--- lxc/clean_lxc.sh
+++ lxc/clean_lxc.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 ###########
-# Clean LXC
+App="Clean LXC"
 ###########
 
-source <(curl -s https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/main.func)
+# Search for main.func primarily localy else source it from the web
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+if [ -f $DIR/../main.func ]; then
+  echo -e "Use local source"
+  source "$DIR/../main.func"
+else
+  echo -e "Use web source"
+  source <(curl -s https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/main.func)
+fi
 
 header_info
+func_app_title
 func_check_if_proxmox
 
 for container in $(pct list | awk '{if(NR>1) print $1}'); do containername=$(pct exec "$container" hostname) &&\
