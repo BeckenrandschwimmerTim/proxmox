3317790c04b66585365037e8a520d82fbc79b9a7
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-21 21:02:27 +0200

    Create powertop_pve03.sh

---
+++ pve-hosts/powertop_pve03.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sleep 30
+
+echo "powersave" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+/sbin/powertop --auto-tune
+echo 'disabled' > '/sys/class/net/enp3s0/device/power/wakeup'
+echo 'disabled' > '/sys/class/net/eno1/device/power/wakeup'
+echo 'disabled' > '/sys/bus/usb/devices/usb1/power/wakeup'
+echo 'disabled' > '/sys/bus/usb/devices/usb2/power/wakeup'
+echo 'disabled' > '/sys/bus/usb/devices/1-4/power/wakeup'
+echo 'disabled' > '/sys/bus/usb/devices/1-5/power/wakeup'
