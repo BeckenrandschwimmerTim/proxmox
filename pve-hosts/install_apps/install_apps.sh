d3b007a22bf74dfd89a300366bee35b9a014264c
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-23 22:00:54 +0200

    Update install_apps.sh

--- pve-hosts/install_apps/install_apps.sh
+++ pve-hosts/install_apps/install_apps.sh
@@ -12,10 +12,10 @@ cd $DIR/misc
 curl https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/main.func -o main.func
 source $DIR/misc/main.func
 
-func_check_if_proxmox
-
 header_info
 func_app_title
+
+func_check_if_proxmox
 
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install mc htop curl powertop acpi-support acpid acpi iperf3 sudo wget -y
