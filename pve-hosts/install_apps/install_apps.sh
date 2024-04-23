fb95eaf4e10eed9a7cfc2911b701475c0514e73a
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-23 21:56:37 +0200

    Update and rename pve-hosts/install_apps.sh to pve-hosts/install_apps/install_apps.sh

---
+++ pve-hosts/install_apps/install_apps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+######################################
+App="Install my needed apps and tools"
+######################################
+
+# Eval script dir
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+mkdir -p $DIR/misc
+cd $DIR/misc
+curl https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/main.func -o main.func
+source $DIR/misc/main.func
+
+header_info
+func_app_title
+
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install mc htop curl powertop acpi-support acpid acpi iperf3 sudo wget -y
