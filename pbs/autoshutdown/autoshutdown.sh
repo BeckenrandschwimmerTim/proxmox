8cf1c8848d4f7bed4aea935247ee6bc9e0d34d48
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-26 08:11:28 +0200

    Update autoshutdown.sh

--- pbs/autoshutdown/autoshutdown.sh
+++ pbs/autoshutdown/autoshutdown.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 
-#Count of times we have queried and there are tasks running
 count=0
-#Number of no-task intervals before we shutdown
 max=30
-#Interval in seconds
 interval=10
 
-
-#Continue until we shutdown
 while true; do
-    #Check if we should shutdown until we see not 
     if [[ $(proxmox-backup-manager task list) ]]; then
-        #Continue to wait
         count=0
     else
-        #There are no tasks running, so increment counter
         count=$((count + 1))
         echo "No tasks running, time until shutdown is $(((max - count) * interval)) seconds"
-        #If we reached the max count, then shutdown
         if  ((count >= max)); then
             echo "Time to shutdown!"
             shutdown now
         fi
     fi
-    #Delay by interval to next check
     sleep "$interval"
 done
 echo "Exiting"
