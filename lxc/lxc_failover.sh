649c7b23733255547cd794e2557f61a345f60938
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-26 17:26:30 +0200

    Update lxc_failover.sh

--- lxc/lxc_failover.sh
+++ lxc/lxc_failover.sh
@@ -1,10 +1,10 @@
 healthstate=$(curl -s 'https://healthchecks.io/b/2/8543ba44-60ad-4c70-9a21-adcdd1d8bb9a.json' | jq -r '.status' | grep up)
 
-if [ "$healthstate" == "" ]
+if [ "$healthstate" == "" ]; then
   echo -e "Nothing found! Is your internet or healthchecks.io down?"
   exit 1
 fi
-if [ "$healthstate" == "up" ]
+if [ "$healthstate" == "up" ]; then
   echo -e "Everything fine! Service seems to be up."
   exit 0
 fi
