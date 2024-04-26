0ad5aadbc1f6ebc7c1823ba5cc48be0a75c2a1a9
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-26 22:13:02 +0200

    Update failover_openhab_master.sh

--- lxc/failover_openhab/failover_openhab_master.sh
+++ lxc/failover_openhab/failover_openhab_master.sh
@@ -4,7 +4,7 @@ master_check="https://hc-ping.com/3bd1985a-1267-422e-83a9-a4776d01b881"
 master_name="openhab-master"
 
 master_state=$(pct list | grep $master_name | grep running | awk '{print $1}')
-if [ -z "$master_state" ]; then
+if [ ! -z "$master_state" ]; then
   echo -e "Everything fine! Service seems to be up."
   curl -m 10 --retry 5 $master_check
 fi
