9ce0bbd4888b5ce8504463b515d360b0ca1edec9
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-26 19:58:59 +0200

    Update slave.sh

--- lxc/failover_openhab/slave.sh
+++ lxc/failover_openhab/slave.sh
@@ -3,5 +3,14 @@
 # Check health state of master
 healthstate=$(curl -s 'https://healthchecks.io/b/2/0e244c1e-5235-4f4a-8ce0-3a3fa3437eaa.json' | jq -r '.status' | grep up)
 
-echo -e "Everything fine! Service seems to be up."
-curl -m 10 --retry 5 https://hc-ping.com/3bd1985a-1267-422e-83a9-a4776d01b881
+if [ "$healthstate" == "up" ]; then
+  echo -e "Everything fine! Service seems to be up."
+  elif [ "$healthstate" == "" ]; then
+    echo -e "Nothing found! Is your internet or healthecks.io down?"
+    elif [ "$healthstate" == "down" ]; then
+      echo -e "Master is down! Starting or keeping slave."
+      curl -m 10 --retry 5 https://hc-ping.com/7c0ae1f0-e9de-4f9b-b0bf-9eb284f1e479
+      # Doing stuff
+    fi
+  fi
+fi
