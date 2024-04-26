a2a28a516fb69caacd88789d9bb3eb3faa610b07
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-26 19:34:52 +0200

    Update and rename lxc/lxc_failover.sh to lxc/failover/openhab/master.sh

---
+++ lxc/failover/openhab/master.sh
@@ -0,0 +1,2 @@
+echo -e "Everything fine! Service seems to be up."
+curl -m 10 --retry 5 https://hc-ping.com/3bd1985a-1267-422e-83a9-a4776d01b881
--- lxc/lxc_failover.sh
+++
@@ -1,9 +0,0 @@
-healthstate_openhab=$(curl -s 'https://healthchecks.io/b/2/0e244c1e-5235-4f4a-8ce0-3a3fa3437eaa.json' | jq -r '.status' | grep up)
-
-if [ "$healthstate_openhab" == "up" ]; then
-  echo -e "Everything fine! Service seems to be up."
-elif
-  if [ "$healthstate_openhab" == "" ]; then
-    echo -e "Nothing found! Is your internet or healthchecks.io down?"
-  fi
-fi
