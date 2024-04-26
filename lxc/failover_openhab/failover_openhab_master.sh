ad763d46d8d2158b091ba3583069a4d548e12bc9
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-04-26 21:52:32 +0200

    Update failover_openhab_master.sh

--- lxc/failover_openhab/failover_openhab_master.sh
+++ lxc/failover_openhab/failover_openhab_master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 master_check="https://hc-ping.com/3bd1985a-1267-422e-83a9-a4776d01b881"
-master_name="openhab_master"
+master_name="openhab-master"
 
 master_state=$(pct list | grep $master_name | awk '{print $1}')
 if [ "$slave_state" == "running" ]; then
