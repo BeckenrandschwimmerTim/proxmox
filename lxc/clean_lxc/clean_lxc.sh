538279fde1a0f8f7701be1c489b03141566922e7
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-08-16 15:20:28 +0200

    Update clean_lxc.sh

--- lxc/clean_lxc/clean_lxc.sh
+++ lxc/clean_lxc/clean_lxc.sh
@@ -10,7 +10,8 @@ func_check_if_proxmox
 
 for container in $(pct list | grep running | awk '{print $1}'); do
     containername=$(pct exec "$container" hostname) &&\
-    echo "CT: $container - $containername" &&\
+    echo -e "Container: $container - $containername" &&\
+    echo -e "Cleaning up logs, Autoclean apt-get, ..." &&\
     pct exec $container -- bash -c "apt-get -y --purge autoremove && apt-get -y autoclean && bash <(curl -fsSL https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/clean.sh) && rm -rf /var/lib/apt/lists/* && apt-get update"
     pct exec $container -- bash -c "docker image prune -a -f >/dev/null 2>&1"
     echo ---; done
