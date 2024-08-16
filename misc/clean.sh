8c7aa3dfc46d55c793ad80abc519bac67986fb2a
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-08-16 15:28:08 +0200

    Update clean.sh

--- misc/clean.sh
+++ misc/clean.sh
@@ -3,21 +3,21 @@
 echo -e "\n Cleaning /var/cache/"
 cache=$(find /var/cache/ -type f)
 if [[ -z "$cache" ]]; then
-  echo -e "It appears there are no cached files on your system"
+  echo -e "${Color_Red}It appears there are no cached files on your system${Color_NC} \n"
   sleep 2
 else
   find /var/cache -type f -delete
-  echo -e "Successfully Removed Cache \n"
+  echo -e "${Color_Green}Successfully Removed Cache${Color_NC} \n"
   sleep 2
 fi
 
 echo -e "Cleaning /var/log/"
 logs=$(find /var/log/ -type f)
 if [[ -z "$logs" ]]; then
-  echo -e "It appears there are no logs on your system"
+  echo -e "${Color_Red}It appears there are no logs on your system${Color_NC} \n"
   sleep 2
 else
   find /var/log -type f -delete
-  echo -e "Successfully Removed Logs \n"
+  echo -e "${Color_Green}Successfully Removed Logs${Color_NC} \n"
   sleep 2
 fi
