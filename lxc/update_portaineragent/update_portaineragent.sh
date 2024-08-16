44feae3d2bb62106407e156c681935de7813c520
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-08-16 12:52:02 +0200

    Create update_portaineragent.sh

---
+++ lxc/update_portaineragent/update_portaineragent.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#######################################################################################
+# Update all PortainerAgents of LXC if tagged with "apptag_portaineragent_as_docker_v1"
+#######################################################################################
+
+App="Update all PortainerAgents"
+
+source <(curl -s https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/main.func)
+
+header_info
+func_check_if_proxmox
+
+for container in $(pct list | awk '{if(NR>1) print $1}'); do portaineragent_as_docker_v1=0 && containername=$(pct exec "$container" hostname) &&\
+    echo "CT: $container - $containername" &&\
+    pct config $container | grep -q "apptag_portaineragent_as_docker_v1" && echo "portaineragent_as_docker_v1: true" && portaineragent_as_docker_v1=1 || echo "portaineragent_as_docker_v1: false" &&\
+    if [ $tools -eq 1 ]; then pct exec $container -- bash -c "cd /sharedfolders/appdata/portainer-agent && docker compose pull && docker compose up -d"; fi
+    echo ---; done
