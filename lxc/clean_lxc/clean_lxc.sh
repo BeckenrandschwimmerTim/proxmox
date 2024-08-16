422953d10aa211dccb07eb6f34cba8801e4826ba
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-08-16 16:10:43 +0200

    Update clean_lxc.sh

--- lxc/clean_lxc/clean_lxc.sh
+++ lxc/clean_lxc/clean_lxc.sh
@@ -15,7 +15,6 @@ for container in $(pct list | grep running | awk '{print $1}'); do
     echo -e "${Color_Green}Cleaning up Logs, Autoclean apt-get, ...${Color_NC}" &&\
     pct exec $container -- bash -c "apt-get -y --purge autoremove && apt-get -y autoclean && bash <(curl -fsSL https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/clean.sh) && rm -rf /var/lib/apt/lists/* && apt-get update" &&\
     echo -e "${Color_Green}Cleaning up docker images no longer used ...${Color_NC}" &&\
-    #pct exec $container -- bash -c "docker image prune -a -f >/dev/null 2>&1" &&\
     pct exec $container -- bash -c "if command -v docker >/dev/null 2>&1; then docker image prune -a -f; exit; fi" &&\
     echo -e "${Color_Green}Container: $container - $containername finished${Color_NC}" &&\
     func_small_line; done
