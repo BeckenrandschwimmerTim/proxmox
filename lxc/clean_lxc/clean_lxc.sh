25db04eec2846138b318e2e22e4d1045e60682f6
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-08-16 15:22:47 +0200

    Update clean_lxc.sh

--- lxc/clean_lxc/clean_lxc.sh
+++ lxc/clean_lxc/clean_lxc.sh
@@ -10,8 +10,10 @@ func_check_if_proxmox
 
 for container in $(pct list | grep running | awk '{print $1}'); do
     containername=$(pct exec "$container" hostname) &&\
-    echo -e "Container: $container - $containername" &&\
-    echo -e "Cleaning up logs, Autoclean apt-get, ..." &&\
+    echo -e "${Color_Green}Container: $container - $containername${Color_NC}" &&\
+    echo -e "${Color_Green}Cleaning up logs, Autoclean apt-get, ...${Color_NC}" &&\
     pct exec $container -- bash -c "apt-get -y --purge autoremove && apt-get -y autoclean && bash <(curl -fsSL https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/clean.sh) && rm -rf /var/lib/apt/lists/* && apt-get update"
+    echo -e "${Color_Green}Cleaning up docker images no longer used ...${Color_NC}" &&\
     pct exec $container -- bash -c "docker image prune -a -f >/dev/null 2>&1"
+    echo -e "${Color_Green}Container: $container - $containername finished${Color_NC}" &&\
     echo ---; done
