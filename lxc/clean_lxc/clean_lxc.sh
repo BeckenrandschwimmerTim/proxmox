d6011accf6949f08e8fe303fbbb5efd9c610d23f
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-08-16 15:32:51 +0200

    Update clean_lxc.sh

--- lxc/clean_lxc/clean_lxc.sh
+++ lxc/clean_lxc/clean_lxc.sh
@@ -10,10 +10,12 @@ func_check_if_proxmox
 
 for container in $(pct list | grep running | awk '{print $1}'); do
     containername=$(pct exec "$container" hostname) &&\
-    echo -e "\n ${Color_Green}Container: $container - $containername${Color_NC}" &&\
+    func_new_line
+    echo -e "${Color_Green}Container: $container - $containername${Color_NC}" &&\
     echo -e "${Color_Green}Cleaning up Logs, Autoclean apt-get, ...${Color_NC}" &&\
     pct exec $container -- bash -c "apt-get -y --purge autoremove && apt-get -y autoclean && bash <(curl -fsSL https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/clean.sh) && rm -rf /var/lib/apt/lists/* && apt-get update" &&\
     echo -e "${Color_Green}Cleaning up docker images no longer used ...${Color_NC}" &&\
     pct exec $container -- bash -c "docker image prune -a -f >/dev/null 2>&1" &&\
     echo -e "${Color_Green}Container: $container - $containername finished${Color_NC}" &&\
+    func_new_line
     func_small_line; done
