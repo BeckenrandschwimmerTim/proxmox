08b18b19378bd775e8711c3eae02affb3382c00c
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-08-16 15:09:30 +0200

    Update clean_lxc.sh

--- lxc/clean_lxc/clean_lxc.sh
+++ lxc/clean_lxc/clean_lxc.sh
@@ -2,22 +2,12 @@
 
 App="Clean LXC"
 
-# Eval script dir
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
 # Source main.func for func_check_if_root and func_check_if_proxmox
 source <(curl -s https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/main.func)
 func_check_if_root
 func_check_if_proxmox
 
-# After func_check_if_proxmox was successfully, continue ...
-mkdir -p $DIR/misc
-cd $DIR/misc
-curl https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/main.func -o main.func
-source $DIR/misc/main.func
-
 header_info
-#func_check_if_proxmox
 func_app_title
 
 for container in $(pct list | grep running | awk '{print $1}'); do
