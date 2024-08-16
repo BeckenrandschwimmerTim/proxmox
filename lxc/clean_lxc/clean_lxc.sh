7572332cd850ce37dc69d1e23d43ad55f31a2def
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-08-16 15:24:23 +0200

    Update clean_lxc.sh

--- lxc/clean_lxc/clean_lxc.sh
+++ lxc/clean_lxc/clean_lxc.sh
@@ -9,7 +9,7 @@ func_check_if_root
 func_check_if_proxmox
 
 for container in $(pct list | grep running | awk '{print $1}'); do
-    containername=$(pct exec "$container" hostname) &&\
+    containername=$(pct exec "\n $container" hostname) &&\
     echo -e "${Color_Green}Container: $container - $containername${Color_NC}" &&\
     echo -e "${Color_Green}Cleaning up logs, Autoclean apt-get, ...${Color_NC}" &&\
     pct exec $container -- bash -c "apt-get -y --purge autoremove && apt-get -y autoclean && bash <(curl -fsSL https://raw.githubusercontent.com/BeckenrandschwimmerTim/proxmox/main/misc/clean.sh) && rm -rf /var/lib/apt/lists/* && apt-get update"
