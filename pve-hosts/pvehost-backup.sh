1cdaf4df267dfe5f399dab98c5b0ebca6c7308a4
BeckenrandschwimmerTim <97850235+BeckenrandschwimmerTim@users.noreply.github.com>
2024-08-28 12:44:26 +0200

    Create pvehost-backup.sh

---
+++ pve-hosts/pvehost-backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export PBS_REPOSITORY=API-TOKEN@IP-OF-PBS:NAME-OF-DATASTORE #API-TOKEN has to be owner of existing host-backups!
+export PBS_PASSWORD=API-TOKEN-PW
+export PBS_FINGERPRINT=FINGERPRINT-OF-PBS
+
+export PBS_REPOSITORY
+export PBS_PASSWORD
+export PBS_FINGERPRINT
+
+proxmox-backup-client backup root.pxar:/ --include-dev /etc/pve
